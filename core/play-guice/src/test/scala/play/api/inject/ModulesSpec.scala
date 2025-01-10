dfb970b0a77937c4491151cb76cd164c1e5118fc
Takeo Sawada <myc.monad@gmail.com>
2025-01-11 00:26:02 +0530

    Update ModulesSpec

--- core/play-guice/src/test/scala/play/api/inject/ModulesSpec.scala
+++ core/play-guice/src/test/scala/play/api/inject/ModulesSpec.scala
@@ -60,6 +60,20 @@ class ModulesSpec extends Specification {
           mod.config must_== conf.underlying
       }
     }
+
+    "load Guice module that take a Config" in {
+      val conf = Configuration(
+        "play.modules.enabled" -> Seq(
+          classOf[JavaGuiceConfigModule].getName
+        )
+      )
+      val located: Seq[Any] = Modules.locate(conf)
+      located.size must_== 1
+      located.head must beLike {
+        case mod: GuiceConfigModule =>
+          mod.config must_== conf.underlying
+      }
+    }
   }
 }
 
@@ -74,3 +88,8 @@ class ScalaGuiceModule(val environment: Environment, val configuration: Configur
 class JavaGuiceConfigModule(val environment: JavaEnvironment, val config: Config) extends AbstractModule {
   override def configure(): Unit = ()
 }
+
+class GuiceConfigModule(val config: Config) extends AbstractModule {
+  override def configure(): Unit = ()
+}
+
