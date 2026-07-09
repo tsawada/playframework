0557c204b2864e54f7dafc5c9ad9ba5f8b17fda6
Matthias Kurz <m.kurz@irregular.at>
2026-07-09 15:36:31 +0200

    Trust RFC 7239 proxy identifiers
    Add play.http.forwarded.trustedProxyIdentifiers so known RFC 7239 obfuscated proxy identifiers can be trusted explicitly.
    
    Continue scanning past configured obfuscated identifiers, keep unknown untrusted, and document the security constraints for this setting.

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -316,7 +316,7 @@ object BuildSettings {
       ProblemFilters.exclude[DirectMissingMethodProblem]("play.filters.https.RedirectHttpsConfiguration.copy"),
       ProblemFilters.exclude[DirectMissingMethodProblem]("play.filters.https.RedirectHttpsConfiguration.this"),
       ProblemFilters.exclude[MissingTypesProblem]("play.filters.https.RedirectHttpsConfiguration$"),
-      // Add trustSingleXForwardedProto to ForwardedHeaderHandlerConfig
+      // ForwardedHeaderHandlerConfig is a private server-internal configuration model.
       ProblemFilters.exclude[DirectMissingMethodProblem](
         "play.core.server.common.ForwardedHeaderHandler#ForwardedHeaderHandlerConfig.apply"
       ),
